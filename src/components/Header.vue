4bdc1831ecf46a74321f45e70752ea3c2cbe1779
klavrenuk <klavrenuk@yandex.ru>
2020-11-08 21:44:27 +0300

    Label UI

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,16 @@
 <template>
     <header>
-        
-
-
+        <Label />
     </header>
 </template>
 
 <script>
+    import Label from './Label.vue'
+
     export default {
         name: 'Header',
+
+        components: {Label},
 
         data() {
             return {
---
+++ src/components/Label.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="label">
+        <span class="label-text">Photo Gallery</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Label'
+    }
+</script>
+
+<style lang="less">
+    .label {
+        margin-bottom: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        & .label-text {
+            padding: 1rem;
+            color: #fff;
+            font-size: 2rem;
+            background: #00B8EA;
+        }
+    }
+</style>
