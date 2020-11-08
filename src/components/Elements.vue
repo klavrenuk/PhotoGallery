b3956f56233f759bf968b8d79e782595e46f0217
klavrenuk <klavrenuk@yandex.ru>
2020-11-08 21:26:11 +0300

    moved main html elements to single file

---
+++ src/components/Elements.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <b-row>
+            <b-col cols="12">
+                <button class="btn">without</button>
+                <button class="btn btn-default">default</button>
+                <button class="btn btn-primary">primary</button>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col cols="12">
+                <a>yandex</a>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col cols="12">
+                <p>honcus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In interdum placerat efficitur. Mauris elit augue, ultrices a velit sed, scelerisque malesuada velit. Etiam tincidunt ultricies ligula vel posuere. Vivamus vestibulum turpis ex, ultricies elementum lorem cursus a. Proin ac mi a est lacinia molestie eget eget magna.</p>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Elements'
+    }
+</script>
