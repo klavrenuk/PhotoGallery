c2d35a436948e30075eafa59aa2613f5de8f50c6
klavrenuk <klavrenuk@yandex.ru>
2022-07-26 17:42:28 +0300

    global styles

--- src/components/demo/Demo.jsx
+++ src/components/demo/Demo.jsx
@@ -23,10 +23,22 @@ export default function Demo() {
 
                 <article>
                     {
-                        buttons.map((button) => {
-                            return <Button color={button}>{button}</Button>
+                        buttons.map((button, index) => {
+                            return <Button key={index} color={button}>{button}</Button>
                         })
                     }
+                </article>
+
+                <article>
+                    <a href={'/'}>link</a>
+                </article>
+
+                <article>
+                    <p>
+                        Quisque vulputate suscipit lectus, ac vulputate purus sollicitudin et. Aenean vulputate tellus eu ipsum suscipit rhoncus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In interdum placerat efficitur. Mauris elit augue, ultrices a velit sed, scelerisque malesuada velit. Etiam tincidunt ultricies ligula vel posuere. Vivamus vestibulum turpis ex, ultricies elementum lorem cursus a. Proin ac mi a est lacinia molestie eget eget magna.
+
+                        Nullam volutpat id est at efficitur. Aliquam erat volutpat. Quisque mollis eu enim ut aliquam. Cras odio erat, condimentum eu tortor et, porta malesuada justo. Sed hendrerit, nibh ut luctus cursus, nisi ex sollicitudin dolor, vitae dapibus felis augue vel justo...
+                    </p>
                 </article>
             </section>
         </Container>
