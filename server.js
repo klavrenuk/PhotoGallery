d087ed78b1f737e65b7f9c80432d2c56a9de554b
klavrenuk <klavrenuk@yandex.ru>
2022-07-29 13:43:16 +0300

    1. fix show images 2. remove images unused 3. change title

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
+const dotenv = require("dotenv");
 
-require('dotenv').config();
-
-console.log(process.env.serverPort);
+dotenv.config({
+    path: './.env'
+})
 
 const app = express(),
     bodyParser = require('body-parser'),
@@ -18,7 +19,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-require('./router')(app);
+require('./server//router')(app);
 
 process.on('uncaughtException', (err) => {
     console.error(err);
@@ -27,7 +28,6 @@ process.on('uncaughtException', (err) => {
 process.on('unhandledrejection', (err) => {
     console.error(err);
 });
-
 
 mongoose.connect(process.env.database,
     {useFindAndModify: false},
