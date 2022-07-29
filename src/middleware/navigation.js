fa1478d20632b91f1bfcbd3b0de4155dca54043d
klavrenuk <klavrenuk@yandex.ru>
2022-07-29 13:19:29 +0300

    remove nav-page: demo

--- src/middleware/navigation.js
+++ src/middleware/navigation.js
@@ -12,11 +12,6 @@ const pages = [
 
 const pagesForView = [
     {
-        title: 'About',
-        path: '/about',
-        component: About
-    },
-    {
         title: 'Home',
         path: '/',
         component: Dashboard
